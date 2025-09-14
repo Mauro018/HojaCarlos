5127e4d96f24324b04a596d0d8cdcc83fd17259d
Mauro017 <carlosmauricioarevalozapata13@gmail.com>
2025-09-14 17:55:46 -0500

    Actualizacion hoja de vida

--- src/components/Referencias.jsx
+++ src/components/Referencias.jsx
@@ -3,7 +3,7 @@ const Referencias = () => {
         {
             name: "Jesus David Zapata Betancourt",
             // position: "",
-            phone: "+57 312 8091832",
+            phone: "312 8091832",
             // email: ""
         }
     ];
@@ -16,7 +16,7 @@ const Referencias = () => {
             <div key={index} className="reference-item">
               <h3>{ref.name}</h3>
               {/* <p>{ref.position}</p> */}
-              {ref.description && <p>{ref.description}</p>}
+              {/* {ref.description && <p>{ref.description}</p>} */}
               <p>Teléfono: {ref.phone}</p>
               {/* {ref.email && <p>Email: {ref.email}</p>} */}
             </div>
