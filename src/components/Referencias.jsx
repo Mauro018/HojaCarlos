c2930733cb154ae009e684f20a66c6a60b72a268
Mauro017 <carlosmauricioarevalozapata13@gmail.com>
2025-09-14 20:48:35 -0500

    Actualizacion 2 hoja de vida

--- src/components/Referencias.jsx
+++ src/components/Referencias.jsx
@@ -5,20 +5,24 @@ const Referencias = () => {
             // position: "",
             phone: "312 8091832",
             // email: ""
+            name2: "Jesus Zapata Muñoz",
+            phone2: "318 6582410",
         }
     ];
 
     return (
         <section className="section">
-        <h2>REFERENCIAS PROFESIONALES</h2>
+        <h2>REFERENCIAS PERSONALES Y FAMILIARES</h2>
         <div className="references-list">
           {references.map((ref, index) => (
             <div key={index} className="reference-item">
-              <h3>{ref.name}</h3>
+              <h3>1. {ref.name}</h3>
               {/* <p>{ref.position}</p> */}
               {/* {ref.description && <p>{ref.description}</p>} */}
               <p>Teléfono: {ref.phone}</p>
               {/* {ref.email && <p>Email: {ref.email}</p>} */}
+              <h3>2. {ref.name2}</h3>
+              <p>Teléfono: {ref.phone2}</p>
             </div>
           ))}
         </div>
